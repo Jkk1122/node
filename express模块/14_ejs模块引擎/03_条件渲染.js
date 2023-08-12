2d3ab1ee5310bd0ed86d68e84213feeaaf77d98f
JiaXinke <12975557+jstarstar@user.noreply.gitee.com>
2023-08-12 18:44:10 +0800

    列表条件渲染

---
+++ express模块/14_ejs模块引擎/03_条件渲染.js
@@ -0,0 +1,8 @@
+const ejs = require('ejs')
+const fs = require('fs')
+let isLogin = true
+
+// 渲染
+let html = fs.readFileSync('./03_条件.html').toString()
+let result = ejs.render(html,{isLogin})
+console.log(result);
